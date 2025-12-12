0e360ef899c7d94ced42375ed06ac5d63ad76c9c
ThePrimeagen <the.primeagen@aol.com>
2025-12-12 14:36:09 +0100

    course files

--- templates/template/includes/_header.php
+++ templates/template/includes/_header.php
@@ -23,19 +23,3 @@
 
 <body class="text-center">
     <div class="container-fluid">
-{% block main %}
-{% endblock %}
-    </div>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="assets/js/jquery.js"></script>
-<script src="assets/js/popper.js"></script>
-<script src="assets/js/bootstrap.js"></script>
-
-<script>
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-</script>
-</body>
-</html>
